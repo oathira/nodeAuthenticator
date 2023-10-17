aee2fe5a3de9e2939de2fa8c443c5a8f580311e1
athira <athiraos1997@gmail.com>
2023-10-17 17:27:10 +0530

    added file directory

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port,function(err){
     if(err){console.log(`error in running the server:${err}`);}
     console.log(`Server is running on port:${port}`);
